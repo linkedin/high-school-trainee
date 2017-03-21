d1559826d838c36d7821da3715b88667a3d76b17
Tiffany Saelinh <tsaelinh@linkedin.com>
2017-03-21 10:26:54 -0700

    typo in weather.js and removing whitespace

--- Week 3/Mini Project Example/weather.js
+++ Week 3/Mini Project Example/weather.js
@@ -8,12 +8,12 @@ $(document).ready(function(){
         location = location.replace(" ", "");
         //turn the selected location into what the API needs
         location = location.replace(",", "%2C%20");
-       
-       //these are the APIs, kept the original just in case
+
+	//these are the APIs, kept the original just in case
         var original = "https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%202487889&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
         var api = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22[[cities%2C%20state]]%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
 
-        //replace the convereted city and state into API
+        //replace the converted city and state into API
         api = api.replace("[[cities%2C%20state]]", location);
 
         $.get(api, function(data, status) {
