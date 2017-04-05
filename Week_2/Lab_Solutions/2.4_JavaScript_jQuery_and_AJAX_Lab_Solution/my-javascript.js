55c40764359060844c33ea1c6a39c93975e15bea
Crystal Chen <cychen@linkedin.com>
2017-04-05 11:54:02 -0700

    Updating all Week 2 materials to latest version

---
+++ Week_2/Lab_Solutions/2.4_JavaScript_jQuery_and_AJAX_Lab_Solution/my-javascript.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+    $("#my-form").submit(function(e) {
+        if (e.preventDefault) e.preventDefault();
+        var data = $("#color").val();
+        if(data == "") {
+            alert("You didn't provide any input!");
+        } else {
+            console.log({"favoriteColor": data});
+        }
+
+        var url = "https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%202487889&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+        $.get(url, function(data, status) {
+            console.log(data);
+            console.log(data["query"]["results"]["channel"]["item"]["condition"]["text"]);
+        },
+        "json");
+
+        return false;
+    });
+});
